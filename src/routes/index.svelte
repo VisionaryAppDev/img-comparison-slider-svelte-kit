0a7caa3bc5abcbc968f85b11711e0622d489df90
Norin <>
2022-07-22 23:55:51 +0700

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { onMount } from 'svelte';
+    onMount(async () => {
+        import ('img-comparison-slider');
+    });
+</script>
+
+<img-comparison-slider value="70" direction="vertical">
+    <img
+            slot="first"
+            style="width: 100%"
+            src="https://img-comparison-slider.sneas.io/demo/images/before.webp"
+    />
+    <img
+            slot="second"
+            style="width: 100%"
+            src="https://img-comparison-slider.sneas.io/demo/images/after.webp"
+    />
+</img-comparison-slider>
